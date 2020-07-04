9c5eb8515ead6428377bcd0f8569225919be5bc8
Ngo Hoang Hiep <hiepnh@magenest.com>
2020-07-04 16:31:54 +0700

    ODOO_SOFA-024: fix print invoice in stock picking

--- advanced_invoice/models/stock_picking.py
+++ advanced_invoice/models/stock_picking.py
@@ -6,10 +6,13 @@ class ModelName (models.Model):
     _inherit = 'stock.picking'
 
     def do_print_invoice(self):
-        invoice = self.env['account.move'].sudo().search(
-            [('sale_order_id', '=', self.sale_id.id), ('state', 'not in', ['cancel'])], limit=1)
-        if invoice:
-            return self.env.ref('advanced_invoice.action_print_advanced_invoice').report_action(invoice)
+        if self.sale_id:
+            invoice = self.env['account.move'].sudo().search(
+                [('sale_order_id', '=', self.sale_id.id), ('state', 'not in', ['cancel'])], limit=1)
+            if len(invoice) > 0:
+                return self.env.ref('advanced_invoice.action_print_advanced_invoice').report_action(invoice)
+            else:
+                raise UserError("Can't find invoice to print")
         else:
             raise UserError("Can't find invoice to print")
     
