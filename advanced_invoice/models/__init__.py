2c8874e2d71cf1bf6032d5f69eeef9be3a5c6fa3
Ngo Hoang Hiep <hiepnh@magenest.com>
2020-08-15 15:23:29 +0700

    ODOO_SOFA-024: add note to sale order line and invoice line, fix customer full address

--- advanced_invoice/models/__init__.py
+++ advanced_invoice/models/__init__.py
@@ -4,4 +4,4 @@ from . import product_template
 from . import sale_coupon_program
 from . import stock_picking
 from . import phone_validation_mixin
-from . import res_users
+from . import res_partner
---
+++ advanced_invoice/models/res_partner.py
@@ -0,0 +1,26 @@
+from odoo import fields, models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    contact_address_full = fields.Char(store=True)
+
+    # @api.depends('street', 'street2', 'state_id', 'x_ward_id', 'x_district_id', 'country_id')
+    # def _compute_complete_full(self):
+    #     for record in self:
+    #         record.contact_address_full = ''
+    #         x= ''
+    #         if record.street:
+    #             x += record.street + ', '
+    #         if record.street2:
+    #             x += record.street2 + ', '
+    #         if record.x_ward_id:
+    #             x += record.x_ward_id.x_name + ', '
+    #         if record.x_district_id:
+    #             x += record.x_district_id.x_name + ', '
+    #         if record.state_id:
+    #             x += record.state_id.name + ', '
+    #         if record.country_id:
+    #             x += record.country_id.name
+    #         record.contact_address_full = x[0:-2]
