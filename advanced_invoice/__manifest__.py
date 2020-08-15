16a223ad48061ce14aa56c975ec01002c6be327b
Ngo Hoang Hiep <hiepnh@magenest.com>
2020-08-15 15:48:20 +0700

    ODOO_SOFA-024: add note to sale order line and invoice line, fix customer full address

--- advanced_invoice/__manifest__.py
+++ advanced_invoice/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '12.0.1.0.1',
+    'version': '12.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product', 'sale', 'sale_stock', 'sale_coupon', 'stock', 'phone_validation'],
