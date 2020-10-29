71ff09caa003012e7a4fc15aeb8ecd5b590b277c
Ngo Hoang Hiep <hiepnh@magenest.com>
2020-10-29 15:15:02 +0700

    ODOO_SOFA-024: update template sms

--- advanced_invoice/models/product_template.py
+++ advanced_invoice/models/product_template.py
@@ -72,7 +72,7 @@ class SaleOrder(models.Model):
             if order.x_studio_mobile:
                 phone = str(order.x_studio_mobile)
                 bid = order.name
-                sms = "Cam on quy khach da dat hang tai SOFACOMPANY. Ma don hang [" + order.name + "] tri gia [" + order.get_format_amount_total() + "]đ. CSKH: 1900636845 (10:00 - 18:00)"
+                sms = "Cam on quy khach da dat hang tai SOFACOMPANY. Ma don hang [" + order.name + "] tri gia [" + order.get_format_amount_total() + "]d. CSKH: 1900636845 (10:00 - 18:00)"
                 url = "https://cloudsms4.vietguys.biz:4438/api/index.php?u=sofacompany&pwd=28ruv&from=SOFACOMPANY&phone=%s&sms=%s&bid=%s&type=8&json=1" % (
                 phone, sms, bid)
                 payload = {}
